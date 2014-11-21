41871259bf7a4ccf2b528427c7389a667b4d628d
Tony Van der Linden <tonyv@bbd.co.za>
2014-11-21 12:02:14 +0200

    interception images

--- Classes/LoginTest.cs
+++ Classes/LoginTest.cs
@@ -3,56 +3,56 @@ using NUnit.Framework;
 
 namespace TestProj.Classes
 {
-    public class LoginTest
+    public class LoginTest : Interfaces.ITestSecurity
     {
-        public void TestLogin(FluentAutomation.Interfaces.IActionSyntaxProvider automater)
+        public void TestLogin(Classes.Browser browserInstance)
         {
             //automater.Assert.Url(Properties.Settings.Default.LogonURL);
 
             //automater.Assert.Url(Properties.Settings.Default.LogonURL);
             //automater.TakeScreenshot("c:\\LogonScreen.jpg");
 
-            automater.Assert.Exists("#txtPassword");
-            automater.Assert.Exists("#txtUserID");
+            browserInstance.Instance.Assert.Exists("#txtPassword");
+            browserInstance.Instance.Assert.Exists("#txtUserID");
 
-            automater.Enter(Properties.Settings.Default.Username).In("#txtUserID");
+            browserInstance.Instance.Enter(Properties.Settings.Default.Username).In("#txtUserID");
             //automater.Enter(Properties.Settings.Default.Username).In("input[name='txtUserID']");
-            automater.Enter(Properties.Settings.Default.Password).In("input[name='txtPassword']");
-            automater.Click("input[name='btnSubmit'");
+            browserInstance.Instance.Enter(Properties.Settings.Default.Password).In("input[name='txtPassword']");
+            browserInstance.Instance.Click("input[name='btnSubmit'");
 
-            automater.Assert.Url(Properties.Settings.Default.URL);
+            browserInstance.Instance.Assert.Url(Properties.Settings.Default.URL);
             //automater.Assert.Exists("li.n29d-nav-create-profile > a");
-            automater.Assert.Text("Tony Van der Linden (tonyv@bbd.co.za)").In("#ctl00_lblPerson");
+            browserInstance.Instance.Assert.Text("Tony Van der Linden (tonyv@bbd.co.za)").In("#ctl00_lblPerson");
         }
 
-        public void TestFailedLogin(FluentAutomation.Interfaces.IActionSyntaxProvider automater)
+        public void TestFailedLogin(Classes.Browser browserInstance)
         {
             //automater.Assert.Url(Properties.Settings.Default.LogonURL);
             //automater.TakeScreenshot("c:\\LogonScreen.jpg");
 
-            automater.Assert.Exists("#txtPassword");
-            automater.Assert.Exists("#txtUserID");
+            browserInstance.Instance.Assert.Exists("#txtPassword");
+            browserInstance.Instance.Assert.Exists("#txtUserID");
 
-            automater.Enter(Properties.Settings.Default.Username).In("#txtUserID");
+            browserInstance.Instance.Enter(Properties.Settings.Default.Username).In("#txtUserID");
             //automater.Enter(Properties.Settings.Default.Username).In("input[name='txtUserID']");
-            automater.Enter("FailedPasswor").In("input[name='txtPassword']");
-            automater.Click("input[name='btnSubmit'");
+            browserInstance.Instance.Enter("FailedPasswor").In("input[name='txtPassword']");
+            browserInstance.Instance.Click("input[name='btnSubmit'");
             //automater.Assert.Exists("li.n29d-nav-create-profile > a");
             //automater.Assert.Url(Properties.Settings.Default.LogonURL);
             //automater.Assert.Text("Invalid credentials - Please try again").In("#lblWarn");
         }
 
-        int logoffImageCounter = 0;
-        public void TestLogoff(FluentAutomation.Interfaces.IActionSyntaxProvider automater)
+        //int logoffImageCounter = 0;
+        public void TestLogoff(Classes.Browser browserInstance)
         {
             //automater.TakeScreenshot(string.Format("c:\\LogoffScreen{0}.jpg",logoffImageCounter++));
 
-            var logoffButton = automater.Find("#ctl00_RightContent_hplLogOff");
+            var logoffButton = browserInstance.Instance.Find("#ctl00_RightContent_hplLogOff");
 
             //automater.Upload(logoffButton, "c:\\logoffbutton.txt");
 
             if (logoffButton != null)
-                automater.Click("#ctl00_RightContent_hplLogOff");
+                browserInstance.Instance.Click("#ctl00_RightContent_hplLogOff");
             //automater.Assert.Exists("li.n29d-nav-create-profile > a");
             //automater.Assert.Url(Properties.Settings.Default.LogonURL);
         }
--- MainTest.cs
+++ MainTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
 using System.Threading;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
 
 namespace TestProj
 {
@@ -8,20 +10,31 @@ namespace TestProj
     public class MainTest : Interfaces.ITestUnit
     {
         //[Test,Description("Function 1"),Repeat(2)]
-        public void TestMethod(Classes.Browser automater)
+        public void TestMethod(Classes.Browser automator)
         {
 
-            automater.Navigate(new Uri(Properties.Settings.Default.LogonURL));
+            IUnityContainer container = new UnityContainer();
+            container.AddNewExtension<Interception>();
+            container.RegisterType<Interfaces.ITestSecurity, Classes.LoginTest>(
+              new Interceptor<InterfaceInterceptor>(),
+              new InterceptionBehavior<Classes.Timer>());
+
+
+            automator.Navigate(new Uri(Properties.Settings.Default.LogonURL));
             //automater.Instance.Wait(5);
             //automater.Instance.TakeScreenshot("StartTestMethod1");
 
-            Classes.LoginTest login = new Classes.LoginTest();
+            Interfaces.ITestSecurity login = container.Resolve<Interfaces.ITestSecurity>();
+            login.TestLogin(automator);
+            login.TestLogoff(automator);
+
+            //Classes.LoginTest login = new Classes.LoginTest();
             //login.TestFailedLogin(automater.Instance);
             //login.TestLogoff(automater.Instance);
-            login.TestLogin(automater.Instance);
+            //login.TestLogin(automator);
             //automater.Instance.Wait(5);
 
-            login.TestLogoff(automater.Instance);
+            //login.TestLogoff(automator);
            // automater.Instance.Wait(5);
 
             //automater.Instance.TakeScreenshot("c:\\EndTestMethod1.jpg");
