9f578b2767e8d3c623ad6bb310662a3ae89dc7bf
Tony Van der Linden <tonyv@bbd.co.za>
2014-11-26 11:30:20 +0200

    Added GeneralApplicationNavigation classes

--- Tests/GeneralApplicationNavigation/GeneralApplicationNavigation.cs
+++ Tests/GeneralApplicationNavigation/GeneralApplicationNavigation.cs
@@ -113,7 +113,37 @@ namespace TestProj.Tests.GeneralApplicationNavigation
         ///   7.2 Please enter <SKU code> and press <enter>
         ///   7.3 Select any icon displayed on the view
         /// TEST OUTPUT:
-        /// 
+        /// 1.
+        ///   1.1 A list of wholesalers are displayed as icons
+        ///   1.2 A list of wholesalers are displayedas icons
+        ///   1.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 2.
+        ///   2.1 A list of wholesalers are displayed as icons
+        ///   2.2 A list of wholesalers are displayedas icons
+        ///   2.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 3.
+        ///   3.1 The product to use for the test is added on the wholesaler list
+        ///   3.2 A list of wholesalers are displayed as icons
+        ///   3.3 A list of wholesalers are displayedas icons
+        ///   3.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 4.
+        ///   4.1 The product to use for the test is added on the wholesaler list
+        ///   4.2 A list of wholesalers are displayed as icons
+        ///   4.3 A list of wholesalers are displayedas icons
+        ///   4.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 5.
+        /// NB. Orders online can be searched within last 30 days, so -1,0, 31 days will be invalid values, between 1- 30 valid days
+        ///   5.1 An order is placed
+        ///   5.2 A list of wholesalers are displayed as icons
+        ///   5.3 A list of wholesalers are displayedas icons
+        ///   5.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 6.
+        ///   6.1 The results  layout displayed a tabular list with the option to toggle between Products, Brand, Order 
+        ///     number or Invoice number results.
+        /// 7.
+        ///   7.1 A list of wholesalers are displayed as icons
+        ///   7.2 A list of wholesalers are displayedas icons
+        ///   7.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
         /// </summary>
         [Test, Description("GridviewSearchOfflineOnline"), Repeat(1)]
         public void GridviewSearchOfflineOnline()
@@ -123,16 +153,73 @@ namespace TestProj.Tests.GeneralApplicationNavigation
             //TODO
         }
 
-
-
         /// <summary>
         /// TEST STEPS:
-        /// 
+        /// 1. Alerts (search only within user’s catalogues)
+        ///   1.1 Please enter <product name)  and press <enter>
+        ///   1.2 Please enter <SKU code> and press <enter>
+        ///   1.3 Select any icon displayed on the view
+        /// 2. Landing page (search only within user’s catalogues)
+        ///   2.1 Please enter <product name)  and press <enter>
+        ///   2.2 Please enter <SKU code> and press <enter>
+        ///   2.3 Select any icon displayed on the view
+        /// 3. Catalogues (search only within user’s catalogues)
+        ///   3.1 Please add a product to wholesaler list  and make sure you save that product name,sku name
+        ///   3.2 Please enter <product name) that you have added above  and press <enter>
+        ///   3.3 Please enter <SKU code> and press <enter>
+        ///   3.4  Select any icon displayed on the view
+        /// 4. Favourites (search only within user’s favourites)
+        ///   4.1 Please add an item in your favourites  and make sure that product name, sku name are saved
+        ///   4.2 Please enter <product name) that you have added above  and press <enter>
+        ///   4.3 Please enter <SKU code> and press <enter>
+        ///   4.4  Select any icon displayed on the view
+        /// 5. Orders Search Offline
+        /// NB. Orders online can be searched within last 30 days, so -1, 0, 31 days will be invalid values, between 1 - 30 valid days
+        ///   5.1 Please place an order
+        ///   5.2 Please Enter  Please enter <product name)  and press <enter>
+        ///   5.3 Please enter <SKU code> and press <enter>
+        ///   5.4 Select any icon displayed on the view
+        /// 6.  Orders search online default 90 days
+        ///   6.1 Please select a date range  and verify that mas retrieves data according to that date range  select period
+        /// 7. Orders general search
+        ///   7.1 Please enter <product name)  and press <enter>
+        ///   7.2 Please enter <SKU code> and press <enter>
+        ///   7.3 Select any icon displayed on the view
         /// TEST OUTPUT:
-        /// 
+        /// 1.
+        ///   1.1 Results of Wholesalers products are displayed as a  List view
+        ///   1.2 Results of Wholesalers products are displayed as a  List view
+        ///   1.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 2.
+        ///   2.1 Results of Wholesalers products are displayed as a  List view
+        ///   2.2 Results of Wholesalers products are displayed as a  List view
+        ///   2.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 3.
+        ///   3.1 The product to use for the test is added on the wholesaler list
+        ///   3.2 Results of Wholesalers products are displayed as a  List view
+        ///   3.3 Results of Wholesalers products are displayed as a  List view
+        ///   3.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 4.
+        ///   4.1 The product to use for the test is added on the wholesaler list
+        ///   4.2 Results of Wholesalers products are displayed as a  List view
+        ///   4.3 Results of Wholesalers products are displayed as a  List view
+        ///   4.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 5.
+        /// NB. Orders online can be searched within last 30 days, so -1,0, 31 days will be invalid values, between 1- 30 valid days
+        ///   5.1 An order is placed
+        ///   5.2 Results of Wholesalers products are displayed as a  List view
+        ///   5.3 Results of Wholesalers products are displayed as a  List view
+        ///   5.4  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
+        /// 6.
+        ///   6.1 The results  layout displayed a tabular list with the option to toggle between Products, Brand, Order 
+        ///     number or Invoice number results.
+        /// 7.
+        ///   7.1 Results of Wholesalers products are displayed as a  List view
+        ///   7.2 Results of Wholesalers products are displayed as a  List view
+        ///   7.3  Results are displayed and grouped per catalogue, with an indicator of the amount of products contained within
         /// </summary>
-        [Test, Description("Method"), Repeat(1)]
-        public void Method()
+        [Test, Description("ListViewSearchOfflineOnline"), Repeat(1)]
+        public void ListViewSearchOfflineOnline()
         {
             browserInstance.Navigate(new Uri("http://aspnet.dev.afrigis.co.za/bopapp"));
             Interfaces.IGeneralApplicationNavigationActions generalApplicationNavigationActions = container.Resolve<Interfaces.IGeneralApplicationNavigationActions>();
