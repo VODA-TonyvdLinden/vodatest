0b933f6b71b3f47cf1167f6f2d0c4af42b17559f
tonyvdlinden <tony.vd.linden@live.co.za>
2014-12-03 11:18:14 +0200

    Moved activate to Helpers

--- Tests/Common/Helpers.cs
+++ Tests/Common/Helpers.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TestProj.Classes;
 
@@ -9,6 +12,8 @@ namespace TestProj.Tests.Common
 {
     public sealed class Helpers
     {
+        IUnityContainer container = new UnityContainer();
+
         static Helpers _instance;
         public static Helpers Instance
         {
@@ -23,7 +28,7 @@ namespace TestProj.Tests.Common
         }
         Helpers()
         {
-
+            container.RegisterType<Interfaces.IActivationActions, Tests.Activation.ActivationActions>(new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<Classes.Timer>(), new InterceptionBehavior<Classes.ScreenCapture>());
         }
 
         public void TestFieldInputValidation(Classes.Browser browserInstance, FluentAutomation.ElementProxy fieldProxy)
@@ -226,6 +231,109 @@ namespace TestProj.Tests.Common
 
             Helpers.Instance.ClickButton(browserInstance, Helpers.Instance.GetProxy(browserInstance, "#product_modal > div > div > div.basketControl.modal-body > div.productControlContainer > form > div.quantityControl > div.increase > button"));
             Helpers.Instance.ClickButton(browserInstance, Helpers.Instance.GetProxy(browserInstance, "#product_modal > div > div > div.basketControl.modal-body > div.productControlContainer > div.finalControls > div.addToBasketContainer > button"));
+        }
+
+        public void Activate(Classes.Browser browserInstance, bool multipleSpazas)
+        {
+            FluentAutomation.ElementProxy msisdn;
+            FluentAutomation.ElementProxy username;
+            FluentAutomation.ElementProxy activationNumber;
+            FluentAutomation.ElementProxy userAlias;
+            FluentAutomation.ElementProxy challengeQuestion;
+            FluentAutomation.ElementProxy challengeAnswer;
+            FluentAutomation.ElementProxy activationNextButton;
+            FluentAutomation.ElementProxy activationErrorMessage;
+            FluentAutomation.ElementProxy otpNextButton;
+            FluentAutomation.ElementProxy otp;
+            FluentAutomation.ElementProxy optResendButton;
+            FluentAutomation.ElementProxy otpErrorMessage;
+
+            browserInstance.Navigate(new Uri("http://aspnet.dev.afrigis.co.za/bopapp/#/activation"));
+            Interfaces.IActivationActions activationAction = container.Resolve<Interfaces.IActivationActions>();
+
+            getActivationControls(browserInstance, out msisdn, out username, out activationNumber, out userAlias, out challengeQuestion, out challengeAnswer, out activationNextButton, out activationErrorMessage);
+
+            if (multipleSpazas)
+                activationAction.TestValidMultiUserDetails(browserInstance, msisdn, username, activationNumber, userAlias, challengeQuestion, challengeAnswer, activationNextButton);
+            else
+                activationAction.TestValidSingleUserDetails(browserInstance, msisdn, username, activationNumber, userAlias, challengeQuestion, challengeAnswer, activationNextButton);
+
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/activation-verifyuser"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/activation-verifyuser");
+
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Exists("#otp"), TimeSpan.FromMinutes(30));
+
+            getOTPControls(browserInstance, out otp, out otpNextButton, out optResendButton, out otpErrorMessage);
+            activationAction.EnterAndVerifyOTPValue(browserInstance, otp, Classes.TestData.Instance.DefaultData.ActivationData.MultiSpazaUser.OTP);
+            Helpers.Instance.ClickButton(browserInstance, otpNextButton);
+
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/activation-managecatalogue"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/activation-managecatalogue");
+
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Exists("#accordion"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Exists("#accordion > div:nth-child(1) > div.title.rightarrow.catalog1.downarrow"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Exists("#accordion > div:nth-child(4) > div.title.rightarrow.catalog4"), TimeSpan.FromMinutes(30));
+
+            var catPageNext = browserInstance.Instance.Find("#alertsView > div.leftBlock.managecatalogue.width862px > div > div.nextBtnSection > button");
+            var catPageUpdate = browserInstance.Instance.Find("#alertsView > div.leftBlock.managecatalogue.width862px > form:nth-child(3) > div.formRow > button");
+
+            int spazaCount = 0;
+            int totalSpazas = 0;
+            getSpazaCounts(browserInstance, out spazaCount, out totalSpazas);
+
+            for (int k = spazaCount; k < totalSpazas; k++)
+            {
+                Helpers.Instance.ClickButton(browserInstance, catPageNext);
+                Thread.Sleep(100);
+            }
+
+            browserInstance.Instance.Assert.Exists("#catalog1");
+            browserInstance.Instance.Assert.Exists("#catalog2");
+            browserInstance.Instance.Assert.Exists("#catalog3");
+            browserInstance.Instance.Assert.Exists("#catalog4");
+
+            Helpers.Instance.ClickButton(browserInstance, catPageUpdate);
+
+            var waiting = browserInstance.Instance.Find("#loading-wating-messages");
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Class("hide").On("#loading-wating-messages"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.Assert.Class("hide").On("#loading-wating-messages");
+            Thread.Sleep(100);
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/main"), TimeSpan.FromMinutes(30));
+            browserInstance.Instance.Assert.Url("http://aspnet.dev.afrigis.co.za/bopapp/#/main");
+            Thread.Sleep(3000);
+            browserInstance.Instance.WaitUntil(() => browserInstance.Instance.Assert.Exists("body > div:nth-child(1) > div > div > ng-include > div > div > div.headerLogo.left > a > img"), TimeSpan.FromMinutes(30));
+
+        }
+        private static void getSpazaCounts(Classes.Browser browserInstance, out int spazaCount, out int totalSpazas)
+        {
+            var SpazaCountElement = Helpers.Instance.GetProxy(browserInstance, "#alertsView > div.leftBlock.managecatalogue.width862px > div > div.txt.ng-binding");
+
+            var working = SpazaCountElement.Element.Text.Split(':')[0];
+            working = working.Replace("Editing catalog for outlets ", "").Replace(" of ", ":");
+            string[] counts = working.Split(':');
+            spazaCount = int.Parse(counts[0]);
+            totalSpazas = int.Parse(counts[1]);
+        }
+
+        private void getActivationControls(Classes.Browser browserInstance, out FluentAutomation.ElementProxy msisdn, out FluentAutomation.ElementProxy username, out FluentAutomation.ElementProxy activationNumber, out FluentAutomation.ElementProxy userAlias, out FluentAutomation.ElementProxy challengeQuestion, out FluentAutomation.ElementProxy challengeAnswer, out FluentAutomation.ElementProxy activationNextButton, out FluentAutomation.ElementProxy errorMessage)
+        {
+            msisdn = browserInstance.Instance.Find("#msisdn");
+            username = browserInstance.Instance.Find("#username");
+            activationNumber = browserInstance.Instance.Find("#activationNumber");
+            userAlias = browserInstance.Instance.Find("#userAlias");
+            challengeQuestion = browserInstance.Instance.Find("#challengeQuestion");
+            challengeAnswer = browserInstance.Instance.Find("#challengeAnswer");
+
+            activationNextButton = browserInstance.Instance.Find("body > div:nth-child(2) > div > div.activationContentMiddle > form > div:nth-child(8) > div > input");
+            errorMessage = browserInstance.Instance.Find("body > div:nth-child(2) > div > div.activationContentMiddle > form > div.ng-binding");
+
+        }
+        private void getOTPControls(Classes.Browser browserInstance, out FluentAutomation.ElementProxy otp, out FluentAutomation.ElementProxy otpNextButton, out FluentAutomation.ElementProxy optResendButton, out FluentAutomation.ElementProxy otpErrorMessage)
+        {
+            otp = browserInstance.Instance.Find("#otp");
+            otpNextButton = browserInstance.Instance.Find("body > div:nth-child(2) > div > div.activationContentMiddle > form > div:nth-child(4) > div > div > input.btn.btn-large.nextBtn.pull-right.purpleButton.ng-scope.ng-binding");
+            optResendButton = browserInstance.Instance.Find("body > div:nth-child(2) > div > div.activationContentMiddle > form > div:nth-child(4) > div > div > input:nth-child(2)");
+            otpErrorMessage = browserInstance.Instance.Find("body > div:nth-child(2) > div > div.activationContentMiddle > form > div.ng-binding");
         }
     }
 }
